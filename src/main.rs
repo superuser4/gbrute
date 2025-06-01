be2f712621d7415b46d63616840d001f23ebd8ef
superuser4 <superuser3344@protonmail.com>
2025-06-01 18:00:27 -0400

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+/// GBrute is a directory and web login bruteforcer
+#[derive(Parser, Debug)]
+#[command(version="0.1.0", about, long_about = None)]
+struct Args {
+    /// Url of the website
+    #[arg(short,long)]
+    url: String,
+    
+    /// Path to worlist
+    #[arg(short,long)]
+    wordlist: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
